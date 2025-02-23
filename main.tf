573268edb92d035beac34425ddad2e41330ff4ab
Kieran Scott <kieran@MacBook-Air.local>
2025-02-23 14:55:35 +0000

    test rds instance

--- main.tf
+++ main.tf
@@ -19,3 +19,50 @@ resource "aws_s3_bucket_acl" "test-bucket-acl" {
   bucket = aws_s3_bucket.test-bucket.id
   acl    = "private"
 }
+
+// RDS INSTANCE
+
+resource "random_password" "test_db_master_password" {
+  length           = 16
+  special         = true
+}
+
+# Store the password in AWS Secrets Manager
+resource "aws_secretsmanager_secret" "test_db_password_secret" {
+  name = "rds/test-master-password"
+}
+
+resource "aws_secretsmanager_secret_version" "test_db_password_version" {
+  secret_id     = aws_secretsmanager_secret.test_db_password_secret.id
+  secret_string = jsonencode({
+    username = "admin"
+    password = random_password.test_db_master_password.result
+  })
+}
+
+# Create RDS instance
+resource "aws_db_instance" "test_rds_instance" {
+  identifier           = "my-rds-instance"
+  engine              = "postgres"
+  engine_version      = "14.3"
+  instance_class      = "db.t3.micro"
+  allocated_storage   = 20
+  storage_encrypted   = true
+  username           = "admin"
+  password           = random_password.test_db_master_password.result
+  publicly_accessible = false
+  skip_final_snapshot = true
+  vpc_security_group_ids = [aws_security_group.db_sg.id]
+}
+
+# Security Group for RDS
+resource "aws_security_group" "db_sg" {
+  name_prefix = "rds-sg"
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] # Restrict this in production!
+  }
+}
