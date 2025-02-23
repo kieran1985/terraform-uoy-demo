a6c3b12d0643ea676fa2a6cb8bfbdfd5cdf48171
Kieran Scott <kieran@MacBook-Air.local>
2025-02-23 14:01:42 +0000

    test s3 bucket

--- main.tf
+++ main.tf
@@ -1,1 +1,21 @@
+// TEST BUCKET 1
+resource "aws_s3_bucket" "test-bucket" {
+  bucket = "test-interview-bucket-s9d3d5x9"
+  tags = {
+    name = "test-bucket"
+  }
+}
 
+resource "aws_s3_bucket_ownership_controls" "test-bucket-ownership" {
+  bucket = aws_s3_bucket.test-bucket.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "test-bucket-acl" {
+  depends_on = [aws_s3_bucket_ownership_controls.test-bucket-ownership]
+  
+  bucket = aws_s3_bucket.test-bucket.id
+  acl    = "private"
+}
